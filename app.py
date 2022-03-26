7c12c66b6dac2ed5047a1efbe1ac5ebc95adb29a
jannatuljannat <57524864+jannatuljannat@users.noreply.github.com>
2022-03-27 03:19:57 +0600

    Update app.py

--- app.py
+++ app.py
@@ -15,23 +15,15 @@ def home_page():
 
 @app.route("/result", methods =['POST']) 
 def result_page():
-    if request.method == 'POST':
-        Pregnancies = float(request.form['pregnancies'])
-        Glucose = float(request.form['glucose'])
-        BP = float(request.form['blood_pressure'])
-        S_Thickness = float(request.form['skin_thickness'])
-        Insulin = float(request.form['insulin'])
-        BMI = float(request.form['bmi'])
-        Diabetes_PF = float(request.form['diabetes_pf'])
-        Age = float(request.form['age']) 
-
-
-# =============================================================================
-#     result = {'preg':Pregnancies, 'glu':Glucose, 'bp':BP, 'st':S_Thickness, 'in':Insulin,'b_m_i':BMI, 'dpf':Diabetes_PF, 'A_g_e':Age}
-#     return jsonify(result)
-# 
-# =============================================================================
-
+    Pregnancies = float(request.form['pregnancies'])
+    Glucose = float(request.form['glucose'])
+    BP = float(request.form['blood_pressure'])
+    S_Thickness = float(request.form['skin_thickness'])
+    Insulin = float(request.form['insulin'])
+    BMI = float(request.form['bmi'])
+    Diabetes_PF = float(request.form['diabetes_pf'])
+    Age = float(request.form['age'])
+         
 
 
     input_query = np.array([[Pregnancies,Glucose,BP,S_Thickness,Insulin,BMI,Diabetes_PF,Age]])
@@ -41,22 +33,8 @@ def result_page():
 
 
 
-# =============================================================================
-#         prediction = model.predict([[Pregnancies,Glucose,BP,S_Thickness,Insulin,BMI,Diabetes_PF,Age]])
-#         
-#         if prediction == 0:
-#             return render_template('index.html', prediction_text="Congrats! You don't have diabetes")
-#         else:
-#             return render_template('index.html', prediction_text="Oops! You have diabetes") 
-#     else:
-#         return render_template('index.html')     
-# =============================================================================
-
-
-
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
     
     
     
